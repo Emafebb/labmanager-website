174bb7cdef548a1ffa3101ca1f86f9547f37b721
Emafebb <emanuele.lucania@gmail.com>
2026-04-08 16:13:59 +0200

    feat: integrate Warehouse section into homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import Features from "@/components/Features";
+import Warehouse from "@/components/Warehouse";
 import WhyLabManager from "@/components/WhyLabManager";
 import Platforms from "@/components/Platforms";
 import ContactForm from "@/components/ContactForm";
@@ -23,6 +24,7 @@ export default function Home() {
       <main id="main-content">
         <Hero />
         <Features />
+        <Warehouse />
         <WhyLabManager />
         <Platforms />
         <ContactForm />
